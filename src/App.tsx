fe2443584819d0d145d5f136370fc44dda33c5e2
AndersonPGS <andersonpgs2001@hotmail.com>
2022-02-25 12:15:44 -0300

    Countdown app created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const COUNTDOWN_INICIAL_TIME_IN_SECONDS = 25 * 60 // 25 minutes
 
 function App() {
 
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INICIAL_TIME_IN_SECONDS);
+
+  useEffect(() => {
+    if (secondsAmount === 0) {
+      alert("Finalizado.");
+      return;
+    }
+
+    setTimeout(() => {
+      setSecondsAmount(state => state - 1);
+    }, 1000);
+  }, [secondsAmount]);
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
   return (
     <div className="App">
-
+      <span id="minutes">{minutes.toString().padStart(2, "0")}</span>
+      <span>:</span>
+      <span id="seconds">{seconds.toString().padStart(2, "0")}</span>
     </div>
   )
 }
